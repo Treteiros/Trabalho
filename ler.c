61630d2e2d2c8405ec8c4b156e4d7c25a1d59e43
nortex3 <nortexy@gmail.com>
2015-03-11 16:44:14 +0000

    32
    2

---
+++ ler.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <clientes.c>
+#include <produtos.c>
+
+int (){
+	FILE *in; LOL
+	int a,i=0;
+    char ficheiro [100];
+    char linha [100];
+    char produtos[200000][100];
+    int conta=0;
+	printf("Introduza o ficheiro: ");
+	scanf("%s",ficheiro);
+	in = fopen(ficheiro, "r" );
+      if( in == NULL )
+       {
+         printf("ERRO: não consigo abrir o ficheiro: %s\n",ficheiro);
+         exit(1);
+       }
+  
+    while(fgets(linha,100,in)){
+    	produtos[][i]=linha;
+    	printf("%s",produtos[i]);
+    	conta++;
+        i++;
+    }
+     
+        
+     printf("Ficheiro %s lido com sucesso, linhas: %d \n",ficheiro,conta);
+     fclose( in );
+
+
+
+}
+
